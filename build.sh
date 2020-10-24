d3448e28c447876a593e0057682f964b4cf3b189
supakeen <cmdr@supakeen.com>
2020-10-24 14:27:46 +0200

    Move bits around.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-nim c --threads:on --app:lib --out:sarafi/_lib/lcg.so src/sarafi/lcg
-nim c --threads:on --app:lib --out:sarafi/_lib/fun.so src/sarafi/fun
+nim c --threads:on --app:lib --out:sarafi/_lib/lcg.so ext/sarafi/lcg
+nim c --threads:on --app:lib --out:sarafi/_lib/fun.so ext/sarafi/fun
